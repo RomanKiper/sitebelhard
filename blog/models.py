fd2d3073921d5e7b810dbf046b2ea799a5ef0156
Kiper Roman <roman_kiper@mail.ru>
2022-12-01 20:11:40 +0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name="название",
+        help_text="Макс. 64 символа"
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name="публикация"
+    )
+    slug = models.SlugField(verbose_name="URL", unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        db_table = "blog_categories"
